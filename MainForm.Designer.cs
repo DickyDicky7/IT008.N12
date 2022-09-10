d676b369deb4efe218ddf63cc0ade80755451175
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2022-09-10 16:26:49 +0700

    update demo media controller + media gallery

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,12 +29,14 @@
         private void InitializeComponent()
         {
             this.btn_load_media = new System.Windows.Forms.Button();
+            this.lbl_1 = new System.Windows.Forms.Label();
             this.media_controller = new IT008.N12_015.MediaController();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // btn_load_media
             // 
-            this.btn_load_media.Location = new System.Drawing.Point(186, 74);
+            this.btn_load_media.Location = new System.Drawing.Point(12, 12);
             this.btn_load_media.Name = "btn_load_media";
             this.btn_load_media.Size = new System.Drawing.Size(75, 23);
             this.btn_load_media.TabIndex = 2;
@@ -42,29 +44,52 @@
             this.btn_load_media.UseVisualStyleBackColor = true;
             this.btn_load_media.Click += new System.EventHandler(this.btn_load_media_Click);
             // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(12, 45);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(78, 13);
+            this.lbl_1.TabIndex = 3;
+            this.lbl_1.Text = "Text goes here";
+            // 
             // media_controller
             // 
-            this.media_controller.Location = new System.Drawing.Point(242, 240);
+            this.media_controller.Location = new System.Drawing.Point(25, 255);
             this.media_controller.Name = "media_controller";
             this.media_controller.Size = new System.Drawing.Size(326, 60);
             this.media_controller.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(348, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 187);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbl_1);
             this.Controls.Add(this.btn_load_media);
             this.Controls.Add(this.media_controller);
             this.Name = "form";
             this.Text = "My Media Player";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private MediaController media_controller;
         private System.Windows.Forms.Button btn_load_media;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
---
+++ MainForm.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IT008.N12_015
+{
+    public partial class form : Form
+    {
+        public form()
+        {
+            InitializeComponent();
+
+            string musicMediaPattern = @"\.mp3$";
+            System.Text.RegularExpressions.Regex musicMediaRegex = new
+            System.Text.RegularExpressions.Regex(musicMediaPattern);
+            Func<string, bool> IsMusicMedia =
+                filePath => musicMediaRegex.IsMatch(filePath);
+
+            lbl_1.Text =
+                String.Join("\n",
+                System.IO.Directory.GetFiles("C:\\Users\\User\\Music")
+                .Where(IsMusicMedia));
+
+            var items = System.IO.Directory.GetFiles("C:\\Users\\User\\Music")
+                .Where(IsMusicMedia);
+
+            Action<object, EventArgs> f(string URL)
+            {
+                Action<object, EventArgs> g = 
+                    new Action<object, EventArgs>((sender, e) =>
+                {
+                    media_controller.LoadMedia(URL);
+                });
+                return g;
+            }
+
+            int y = 0;
+
+            foreach (string item in items)
+            {
+                Button m = new Button();
+                m.Text = item.Substring(20);
+                m.Click += new EventHandler(f(item));
+                m.Location = new Point(0, y);
+                groupBox1.Controls.Add(m);
+                y += m.Height;
+            }
+
+        }
+
+        private void btn_load_media_Click(object sender, EventArgs e)
+        {
+            media_controller.LoadMedia(
+                "C:\\Users\\User\\Music\\Circus10529-VA-5631008.mp3"
+            );
+        }
+    }
+}
