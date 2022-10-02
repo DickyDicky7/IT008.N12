2e2b2a90d6d70d6c6578ca2c2c5f7533cec88278
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2022-10-02 21:50:42 +0700

    add SoundCloud components, Example (Dummy)

--- SoundCloudSearchBox.cs
+++ SoundCloudSearchBox.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Drawing;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace IT008.N12_015
 {
@@ -15,6 +16,29 @@ namespace IT008.N12_015
         public SoundCloudSearchBox()
         {
             InitializeComponent();
+
+            Timer = new System.Threading.Timer(
+                new System.Threading.TimerCallback((s) => {
+                    if (LastMod != null)
+                    {
+                        if (DateTime.Now.Subtract(LastMod.Value) >= TimeSpan.FromSeconds(3))
+                        {
+                            TextBox.BeginInvoke((MethodInvoker)delegate ()
+                            {
+                                TextBox.Text = "";
+                            });
+                                LastMod = null;
+                        }
+                    }
+                }), null,0,1);
+
+            TextBox.TextChanged += new EventHandler((s, e) =>
+            {
+                LastMod = DateTime.Now;
+            });
         }
+
+        private DateTime? LastMod;
+        private System.Threading.Timer Timer;
     }
 }
