9903d2dfbb0784dca741dd535b8cfe0d93fab657
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2022-10-22 20:26:18 +0700

    .

--- src/MediaController/MediaController.Designer.cs
+++ src/MediaController/MediaController.Designer.cs
@@ -50,9 +50,9 @@
             this.TrackBar.FillColor = System.Drawing.Color.LightGray;
             this.TrackBar.HoverState.Parent = this.TrackBar;
             this.TrackBar.Location = new System.Drawing.Point(0, 0);
-            this.TrackBar.MaximumSize = new System.Drawing.Size(1000, 50);
+            this.TrackBar.MaximumSize = new System.Drawing.Size(1040, 50);
             this.TrackBar.Name = "TrackBar";
-            this.TrackBar.Size = new System.Drawing.Size(1000, 50);
+            this.TrackBar.Size = new System.Drawing.Size(1040, 50);
             this.TrackBar.TabIndex = 10;
             this.TrackBar.ThumbColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(24)))), ((int)(((byte)(27)))));
             this.TrackBar.Scroll += new System.Windows.Forms.ScrollEventHandler(this.TrackBar_Scroll);
@@ -78,10 +78,10 @@
             this.Panel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Panel.ForeColor = System.Drawing.Color.Transparent;
             this.Panel.Location = new System.Drawing.Point(0, 50);
-            this.Panel.MaximumSize = new System.Drawing.Size(1000, 100);
+            this.Panel.MaximumSize = new System.Drawing.Size(1040, 100);
             this.Panel.Name = "Panel";
             this.Panel.ShadowDecoration.Parent = this.Panel;
-            this.Panel.Size = new System.Drawing.Size(1000, 100);
+            this.Panel.Size = new System.Drawing.Size(1040, 100);
             this.Panel.TabIndex = 11;
             // 
             // DurationLabel
@@ -297,9 +297,9 @@
             this.Controls.Add(this.TrackBar);
             this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.Color.Transparent;
-            this.MaximumSize = new System.Drawing.Size(1000, 150);
+            this.MaximumSize = new System.Drawing.Size(1040, 150);
             this.Name = "MediaController";
-            this.Size = new System.Drawing.Size(1000, 150);
+            this.Size = new System.Drawing.Size(1040, 150);
             this.Panel.ResumeLayout(false);
             this.ResumeLayout(false);
 
