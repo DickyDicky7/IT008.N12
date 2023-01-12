f8b425fece3b36ab82eb2539a9b1db313a792476
JustVvu <nguyenvanvu065@gmail.com>
2023-01-12 20:23:42 +0700

    Update VideoItem

--- Source/VideoItem/VideoItem.Designer.cs
+++ Source/VideoItem/VideoItem.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.siticonePanel1 = new Siticone.Desktop.UI.WinForms.SiticonePanel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.TitleLB = new System.Windows.Forms.Label();
             this.ThumbnailBox = new Siticone.Desktop.UI.WinForms.SiticonePictureBox();
+            this.TitleLB = new System.Windows.Forms.Label();
             this.siticonePanel1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.ThumbnailBox)).BeginInit();
@@ -43,38 +43,26 @@
             this.siticonePanel1.BorderRadius = 10;
             this.siticonePanel1.BorderThickness = 1;
             this.siticonePanel1.Controls.Add(this.tableLayoutPanel1);
-            this.siticonePanel1.Location = new System.Drawing.Point(20, 20);
+            this.siticonePanel1.Location = new System.Drawing.Point(10, 10);
             this.siticonePanel1.Name = "siticonePanel1";
-            this.siticonePanel1.Size = new System.Drawing.Size(960, 120);
+            this.siticonePanel1.Size = new System.Drawing.Size(290, 290);
             this.siticonePanel1.TabIndex = 0;
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
-            this.tableLayoutPanel1.Controls.Add(this.TitleLB, 1, 0);
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.Controls.Add(this.ThumbnailBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TitleLB, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(960, 120);
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(290, 290);
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tableLayoutPanel1_MouseClick);
-            // 
-            // TitleLB
-            // 
-            this.TitleLB.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.TitleLB.AutoSize = true;
-            this.TitleLB.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.TitleLB.Location = new System.Drawing.Point(243, 47);
-            this.TitleLB.Name = "TitleLB";
-            this.TitleLB.Size = new System.Drawing.Size(49, 25);
-            this.TitleLB.TabIndex = 1;
-            this.TitleLB.Text = "Title";
             // 
             // ThumbnailBox
             // 
@@ -83,10 +71,22 @@
             this.ThumbnailBox.InitialImage = null;
             this.ThumbnailBox.Location = new System.Drawing.Point(3, 3);
             this.ThumbnailBox.Name = "ThumbnailBox";
-            this.ThumbnailBox.Size = new System.Drawing.Size(234, 114);
+            this.ThumbnailBox.Size = new System.Drawing.Size(284, 197);
             this.ThumbnailBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ThumbnailBox.TabIndex = 2;
             this.ThumbnailBox.TabStop = false;
+            // 
+            // TitleLB
+            // 
+            this.TitleLB.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TitleLB.AutoSize = true;
+            this.TitleLB.Font = new System.Drawing.Font("Segoe UI Semibold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.TitleLB.Location = new System.Drawing.Point(120, 234);
+            this.TitleLB.Name = "TitleLB";
+            this.TitleLB.Size = new System.Drawing.Size(49, 25);
+            this.TitleLB.TabIndex = 1;
+            this.TitleLB.Text = "Title";
+            this.TitleLB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // VideoItem
             // 
@@ -95,7 +95,7 @@
             this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.siticonePanel1);
             this.Name = "VideoItem";
-            this.Size = new System.Drawing.Size(1000, 160);
+            this.Size = new System.Drawing.Size(310, 310);
             this.siticonePanel1.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -108,7 +108,7 @@
 
         private Siticone.Desktop.UI.WinForms.SiticonePanel siticonePanel1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label TitleLB;
         private Siticone.Desktop.UI.WinForms.SiticonePictureBox ThumbnailBox;
+        private System.Windows.Forms.Label TitleLB;
     }
 }
