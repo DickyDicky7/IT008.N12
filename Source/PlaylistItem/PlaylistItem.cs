5b109d03accb4b7730bc7c68188b13671192969a
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-01-23 21:45:17 +0700

    update PlaylistItem

--- Source/PlaylistItem/PlaylistItem.cs
+++ Source/PlaylistItem/PlaylistItem.cs
@@ -23,28 +23,32 @@ namespace MyMediaPlayer
             InitializeComponent();
             this.URL = URL;
             Render();
-            DataGridView.CellMouseClick += (sender, e) =>
+            DataGridView.CellMouseClick += DataGridView_CellMouseClick;
+        }
+
+        private void DataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.RowIndex is -1)
             {
-                if (e.RowIndex is -1)
-                    return;
-                else
-                if (e.Button is MouseButtons.Left)
-                {
-                    GlobalReferences.MediaController.LoadMediaItemList(this);
-                    #region Testing
-                    //Old code
-                    //UpdateCurrentIndex(null);
-                    CurrentIndex = e.RowIndex;
-                    #endregion
-                    Play();
-                }
-                else
-                if (e.Button is MouseButtons.Right)
-                {
-                    RowIndex = e.RowIndex;
-                    ChildMenu.Show(MousePosition);
-                }
-            };
+                return;
+            }
+            else
+            if (e.Button is MouseButtons.Left)
+            {
+                GlobalReferences.MediaController.LoadMediaItemList(this);
+                #region Testing
+                //Old code
+                //UpdateCurrentIndex(null);
+                CurrentIndex = e.RowIndex;
+                #endregion
+                Play();
+            }
+            else
+            if (e.Button is MouseButtons.Right)
+            {
+                RowIndex = e.RowIndex;
+                ChildMenu.Show(MousePosition);
+            }
         }
 
         private List<string> Paths;
@@ -145,14 +149,29 @@ namespace MyMediaPlayer
 
         public Action Clear => () =>
         {
+            RenamePlaylistToolStripMenuItem.Click -= RenamePlaylistToolStripMenuItem_Click;
+            ChildRemoveToolStripMenuItem.Click -= ChildRemoveToolStripMenuItem_Click;
+            RemoveToolStripMenuItem.Click -= RemoveToolStripMenuItem_Click;
+            DataGridView.CellMouseClick -= DataGridView_CellMouseClick;
+            PlayButton.Click -= PlayButton_Click;
+            Panel.MouseClick -= Panel_MouseClick;
+
             for (int k = 0; k < DataGridView.Rows.Count; k++) DataGridView.Rows[k].Dispose();
             DataGridView.Rows.Clear();
 
-            ChildRemoveToolStripMenuItem.Click += null;
-            RemoveToolStripMenuItem.Click += null;
-            DataGridView.CellMouseClick += null;
-            PlayButton.Click += null;
-            Panel.MouseClick += null;
+            RenamePlaylistToolStripMenuItem.Dispose();
+            ChildRemoveToolStripMenuItem.Dispose();
+            RemoveToolStripMenuItem.Dispose();
+
+            RenamePlaylistToolStripMenuItem = null;
+            ChildRemoveToolStripMenuItem = null;
+            RemoveToolStripMenuItem = null;
+
+            Menu.Dispose();
+            ChildMenu.Dispose();
+
+            Menu = null;
+            ChildMenu = null;
         };
 
         public Action PlayBack => () =>
