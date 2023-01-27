82ddd3ee0eb137a1cc83f4ea22ff25fee13ff501
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2023-01-27 22:41:25 +0700

    update installer

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -263,6 +263,16 @@ namespace MyMediaPlayer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap nothing {
+            get {
+                object obj = ResourceManager.GetObject("nothing", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap pause {
             get {
                 object obj = ResourceManager.GetObject("pause", resourceCulture);
